7d916a26d450b09dcd9c6eca73bc0549ea3a9871
songwooju <37572006+songwooju@users.noreply.github.com>
2023-05-12 16:37:42 +0900

    Boss die

---
+++ Assets/Source/Scripts/BossController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    public int health = 100; // 보스의 체력
+  
+    // 보스의 체력을 감소시키는 함수
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
--- Assets/Source/Scripts/BulletController.cs
+++ Assets/Source/Scripts/BulletController.cs
@@ -7,6 +7,8 @@ public class BulletController : MonoBehaviour
     public float speed;
     public Transform bossTransform;
 
+    private int damage = 10; // 총알의 데미지
+
     // Start is called before the first frame update
     void Start()
     {
@@ -39,7 +41,16 @@ public class BulletController : MonoBehaviour
     {
         if (collision.CompareTag("Boss"))
         {
-            Destroy(gameObject);
+            BossController boss = collision.GetComponent<BossController>(); // 보스의 BossController 스크립트를 가져옴
+            if (boss != null) // BossController 스크립트가 존재할 경우
+            {
+                boss.TakeDamage(damage); // 보스의 체력을 감소시킴
+                if (boss.health <= 0) // 보스의 체력이 0 이하일 경우
+                {
+                    Destroy(collision.gameObject); // 보스를 파괴함
+                }
+            }
+            Destroy(gameObject); // 총알을 파괴함
         }
     }
 }
